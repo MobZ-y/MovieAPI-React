08036b341b3955ef3d11c5b4e879d7ecffe17c98
unknown <romain.nourrydu66@gmail.com>
2023-01-27 01:15:05 +0100

    d

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,31 +1,45 @@
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
 import CardPerson from "./CardPerson";
 
 const Actors = () => {
   const [data, setData] = useState([]);
+  const [url, setUrl] = useState(
+    "https://api.themoviedb.org/3/trending/person/week?api_key=dc4fa11dbb0888468121f0e93ac98077"
+  );
+  const contentTypes = [
+    {
+      name: "Semaine",
+      url: "https://api.themoviedb.org/3/trending/person/week?api_key=dc4fa11dbb0888468121f0e93ac98077",
+    },
+    {
+      name: "Jour",
+      url: "https://api.themoviedb.org/3/trending/person/day?api_key=dc4fa11dbb0888468121f0e93ac98077",
+    },
+  ];
+
   useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/trending/person/week?api_key=dc4fa11dbb0888468121f0e93ac98077"
-      )
-      .then((res) => setData(res.data.results));
-  }, []);
+    axios.get(url).then((res) => setData(res.data.results));
+  }, [url]);
+
+  const handleNavLinkClick = (contentType) => {
+    setUrl(contentTypes.find((type) => type.name === contentType).url);
+  };
 
   return (
     <div>
       <div className="trendingOnPerson">
         <div className="SwitchDisco">
-          <h3>
-            <a href="" id="Week">
-              Semaine
-            </a>
-          </h3>
-          <h3>
-            <a href="" id="Day">
-              Jour
-            </a>
-          </h3>
+          {contentTypes.map((contentType) => (
+            <NavLink
+              key={contentType.name}
+              id={contentType.name}
+              onClick={() => handleNavLinkClick(contentType.name)}
+            >
+              <h3>{contentType.name}</h3>
+            </NavLink>
+          ))}
         </div>
         <div className="carouselPerson">
           <div className="trendingPerson">
