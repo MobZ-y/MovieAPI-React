2936dbe8703b6069ee5510c159cfe37257b34e7a
unknown <romain.nourrydu66@gmail.com>
2023-02-07 01:57:31 +0100

    styles add

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,24 +7,25 @@ const SearchTest = () => {
   return (
     <div>
       <div>
-        <div className="filter-container">
-          <div className="Titlte-filter-container">
-            <h2>Bienvenue !</h2>
-          </div>
-          <div className="input-search">
-            <input
-              type="text"
-              id="inputSearch"
-              placeholder="Rechercher, acteurs, réalisateur"
-              onChange={(e) => setPeople(e.target.value)}
-            />
-            <NavLink to={`./SearchPage/${People}`}>
-              <button>Search</button>
-            </NavLink>
+        <div className="content">
+          <div className="filter-container">
+            <div className="Titlte-filter-container">
+              <h2>Bienvenue !</h2>
+            </div>
+            <div className="input-search">
+              <input
+                type="text"
+                id="inputSearch"
+                placeholder="Rechercher, acteurs, réalisateur"
+                onChange={(e) => setPeople(e.target.value)}
+              />
+              <NavLink to={`./SearchPage/${People}`}>
+                <input type="submit" value="search" id="inputFinder" />
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
-      <div className="people"></div>
     </div>
   );
 };
