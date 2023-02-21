822f5f1393081724b3fe75d457f4e818be6f0aad
unknown <romain.nourrydu66@gmail.com>
2023-02-21 02:01:47 +0100

    séparation de movie et tv

--- src/components/MoviesAndTV.js
+++ src/components/MoviesAndTV.js
@@ -8,19 +8,22 @@ const MoviesAndTV = () => {
   const [data, setData] = useState([]);
   const [Movie, setMovie] = useState([]);
   const [Tv, setTv] = useState([]);
-  const [url, setUrl] = useState(
-    "https://api.themoviedb.org/3/trending/all/day?api_key=dc4fa11dbb0888468121f0e93ac98077&page=1"
-  );
-  const contentTypes = [
-    {
-      name: "Populaires",
-      url: "https://api.themoviedb.org/3/trending/all/day?api_key=dc4fa11dbb0888468121f0e93ac98077&page=1",
-    },
-    {
-      name: "Populaires TV",
-      url: "https://api.themoviedb.org/3/tv/popular?api_key=dc4fa11dbb0888468121f0e93ac98077",
-    },
-  ];
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/trending/all/day?api_key=dc4fa11dbb0888468121f0e93ac98077"
+      )
+      .then((res) => setData(res.data.results));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/tv/popular?api_key=dc4fa11dbb0888468121f0e93ac98077"
+      )
+      .then((res) => setTv(res.data.results));
+  }, []);
 
   useEffect(() => {
     axios
@@ -30,42 +33,25 @@ const MoviesAndTV = () => {
       .then((res) => setMovie(res.data.results));
   }, []);
 
-  useEffect(() => {
-    axios.get(url).then((res) => {
-      if (url.includes("tv")) {
-        setTv(res.data.results);
-      } else {
-        setData(res.data.results);
-      }
-    });
-  }, [url]);
-
-  const handleNavLinkClick = (contentType) => {
-    const content = contentTypes.find((type) => type.name === contentType);
-    setUrl(content.url);
-  };
-
   return (
     <div>
       <div className="movies-trending">
-        <div className="Switch">
-          {contentTypes.map((contentType) => (
-            <NavLink
-              key={contentType.name}
-              id={contentType.name}
-              onClick={() => handleNavLinkClick(contentType.name)}
-            >
-              <h3>{contentType.name}</h3>
-            </NavLink>
-          ))}
-        </div>
         <div className="carousel">
+          <h3>Tendances de la semaine</h3>
           <div className="trending">
-            {url.includes("tv")
-              ? Tv.map((tvShow, index) => <CardTV key={index} Tv={tvShow} />)
-              : data.map((movie, index) => (
-                  <CardMovie key={index} Movies={movie} />
-                ))}
+            {data.map((movie2, index) => (
+              <CardMovie key={index} Movies={movie2} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="tv-trending">
+        <div className="carousel-tv">
+          <h3>Emisions télévisé Populaires</h3>
+          <div className="trending">
+            {Tv.map((movie2, index) => (
+              <CardMovie key={index} Movies={movie2} />
+            ))}
           </div>
         </div>
       </div>
---
+++ src/components/TVhome.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TVhome = () => {
+  return <div></div>;
+};
+
+export default TVhome;
