5d7c9df672e21615a1d6c5e335cd339a2e5c1448
unknown <romain.nourrydu66@gmail.com>
2023-03-10 01:31:42 +0100

    d

--- frontend/src/pages/Films.js
+++ frontend/src/pages/Films.js
@@ -29,6 +29,14 @@ const Films = () => {
       .then((res) => setPopular(res.data.results));
   };
 
+  const handleSoonclick = () => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=dc4fa11dbb0888468121f0e93ac98077&language=en-US&page=1`
+      )
+      .then((res) => setPopular(res.data.results));
+  };
+
   console.log(popular);
   return (
     <div>
@@ -40,23 +48,16 @@ const Films = () => {
             <button className="btnSort" onClick={handlePopularClick}>
               Populaires
             </button>
-            <button
-              className="btnSort"
-              id="TopRated"
-              onClick={handleTopRatedClick}
-            >
+            <button className="btnSort" onClick={handleTopRatedClick}>
               Le mieux notés
             </button>
-            <button className="btnSort" id="maxToMin">
-              Tri décroissant
-            </button>
-            <button className="btnSort" id="alpha">
-              Tri Alphabétique
+            <button className="btnSort" onClick={handleSoonclick}>
+              Prochainement
             </button>
           </div>
           <div className="famous-films">
             {popular.map((popular) => (
-              <div className="card-films">
+              <div className="card-pre">
                 <div className="card-films">
                   <div className="profile-films">
                     <NavLink to={`/Movie/${popular.id}`}>
@@ -66,9 +67,27 @@ const Films = () => {
                       />
                     </NavLink>
                   </div>
-                  <div className="profile-films">
+
+                  <div className="profile-films-meta">
                     <p className="name">{popular.original_title}</p>
                     <p className="sub"></p>
+                    <div
+                      id="canvas"
+                      className={
+                        popular.vote_average === 0
+                          ? "grey"
+                          : popular.vote_average > 6
+                          ? "green"
+                          : popular.vote_average >= 2 &&
+                            popular.vote_average < 3
+                          ? "red"
+                          : "orange"
+                      }
+                    >
+                      {popular.vote_average === 0
+                        ? ""
+                        : Math.floor(popular.vote_average * 10).toFixed(0)}
+                    </div>
                   </div>
                 </div>
               </div>
